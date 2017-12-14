3f54ab9d137700bbe980f72385a2599dad15d300
Lenovo <Lenovo@DESKTOP-80UM70A>
2017-12-14 23:19:08 +0200

    Средний рост

--- ConsoleApplication9/Program.cs
+++ ConsoleApplication9/Program.cs
@@ -10,6 +10,25 @@ namespace ConsoleApplication9
     {
         static void Main(string[] args)
         {
+            byte students;            
+            double all = 0;
+
+            Console.WriteLine("Введите количество студентов: ");
+            students = byte.Parse(Console.ReadLine());
+            byte[] heightArr = new byte[students];
+
+            Console.WriteLine("Поочередно вводите рост каждого студента: ");
+            for (int i = 0; i < heightArr.Length; i++)
+            {
+                Console.Write($"{i+1}) ");
+                heightArr[i] = byte.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < heightArr.Length; i++)                            
+                all += heightArr[i];  
+                      
+            Console.WriteLine($"Средний рост в группе: {Math.Round(all/students, 2)}");
+            
         }
     }
 }
